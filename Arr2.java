35406e66a911e9f9d0eb8840c4023aca1fb10959
KIRTISENGAR95 <155946494+KIRTISENGAR95@users.noreply.github.com>
2024-01-11 18:47:21 +0530

    Add files via upload

---
+++ Arr2.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Arr2{
+    public static void main(String [] args){
+        int arr [] = {12,45,56,78,99};
+        int n=arr.length;
+        for(int i=0;i<n;i++){
+            System.out.println(arr[i]);
+        }
+
+    }
+}
