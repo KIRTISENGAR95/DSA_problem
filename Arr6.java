d2a1e014081e0e6d7abf73c19174bcea8f49ad1d
KIRTISENGAR95 <155946494+KIRTISENGAR95@users.noreply.github.com>
2024-01-11 18:49:48 +0530

    Add files via upload

---
+++ Arr6.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Arr6{
+    public static void main(String [] args){
+        int arr []={14,16,18,44,66};
+        int x=68;
+        int n=arr.length;
+        for(int i=0;i<n;i++){
+            if(arr[i]==x){
+                System.out.println("yes");
+            }
+            
+        }
+        
+        
+        
+    }
+}
