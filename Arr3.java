334b9826461b23f2bf51a5266db16545f3f67543
KIRTISENGAR95 <155946494+KIRTISENGAR95@users.noreply.github.com>
2024-01-11 18:47:48 +0530

    Add files via upload

---
+++ Arr3.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Arr3{
+    public static void main(String [] args){
+        int arr []={14,16,18,44,66};
+        int n=arr.length;
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum=sum+arr[i];
+
+        }
+        System.out.println(sum);
+    }
+}
