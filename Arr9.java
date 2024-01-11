cc5bc1c8dccc820419df802953c10869dff31042
KIRTISENGAR95 <155946494+KIRTISENGAR95@users.noreply.github.com>
2024-01-11 18:49:20 +0530

    Add files via upload

---
+++ Arr9.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Arr9{
+    public static void main(String [] args){
+        int arr []={14,16,18,44,66};
+        int n=arr.length;
+        for(int i=n-1;i>0;i--){
+            System.out.println(arr[i]);
+        }
+    }
+}
